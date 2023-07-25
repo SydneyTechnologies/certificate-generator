0bbd40cda3d65a8e79a99622de62ac45c06e8358
Sydney Idundun <76040196+SydneyTechnologies@users.noreply.github.com>
2023-07-25 23:12:59 +0100

    fix: changed the certificate generator code to accomodate signatures

--- utils/cert_generator.py
+++ utils/cert_generator.py
@@ -4,6 +4,7 @@ import numpy as np
 import urllib.request
 import io
 
+
 url = 'https://mcs-backend.up.railway.app/resources/'  # Replace with the URL of the image you want to read
 
 
@@ -18,7 +19,7 @@ url = 'https://mcs-backend.up.railway.app/resources/'  # Replace with the URL of
 #track = f"{user.track}"            #Input from webpage
 
 # Three other files must be in the directory:
-# 1 - the image of the certificate template, 'cert_template.png'
+# 1 - the image of the certificate template, 'cert_template.jpeg'
 # 2 - Font 1 file NotoSans-CondensedSemiBold.ttf
 # 3 - Font 2 file NotoSans-Italic.ttf
 
@@ -39,7 +40,7 @@ def generate_cert(name, track):
     line_3 = 'hard work, and commitment have led you to achieve this significant'
     line_4 = 'milestone.  Congratulations on your accomplishment!'
 
-    req = urllib.request.urlopen(url+'cert_template.png')
+    req = urllib.request.urlopen(url+'cert_template.jpeg')
     arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
     template = cv2.imdecode(arr, -1)
     if not template.any(): 
@@ -58,23 +59,23 @@ def generate_cert(name, track):
 
     # Load the font from the downloaded file
 
-    font1 = ImageFont.truetype(font_one_path, 100)  
-    font2 = ImageFont.truetype(font_two_path, 40)  
+    font1 = ImageFont.truetype(font_one_path, 250)  
+    font2 = ImageFont.truetype(font_two_path, 100)  
 
     #Putting text into img
-    h = 150
-    w = 420
+    h = 500
+    w = 1275
 
     if type(name) == list:
         draw.text((h, w), name[0].upper(), font=font1, fill='#646464')  
-        draw.text((h, w+120), name[1].upper(), font=font1, fill='#646464')  
+        draw.text((h, w+325), name[1].upper(), font=font1, fill='#646464')  
     else:
-        draw.text((h, w+50), name.upper(), font=font1, fill='#646464')
+        draw.text((h, w+200), name.upper(), font=font1, fill='#646464')
  
-    draw.text((h, w+120+160), line_1, font=font2, fill='#646464')  
-    draw.text((h, w+120+160+70), line_2, font=font2, fill='#646464') 
-    draw.text((h, w+120+160+70+70), line_3, font=font2, fill='#646464')  
-    draw.text((h, w+120+160+70+70+70), line_4, font=font2, fill='#646464')  
+    draw.text((h, w+300+500), line_1, font=font2, fill='#646464')  
+    draw.text((h, w+300+500+175), line_2, font=font2, fill='#646464') 
+    draw.text((h, w+300+500+175+175), line_3, font=font2, fill='#646464')  
+    draw.text((h, w+300+500+175+175+175), line_4, font=font2, fill='#646464')  
 
     return cv2.cvtColor(np.array(template), cv2.COLOR_RGB2BGR)   #Converting back to RGB
 
