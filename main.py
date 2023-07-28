274ae6b4c2f72817c3bcb662cdff6dc970580fe6
Sydney Idundun <76040196+SydneyTechnologies@users.noreply.github.com>
2023-07-28 08:34:58 +0100

    fixed some changes

--- main.py
+++ main.py
@@ -67,6 +67,8 @@ def validate(email:str = Body(), track:str = Body(), db=Depends(crud.get_db)):
 @app.get("/api/get_certificate/{email}", tags=["Certificate"], summary="Downloads the certificate for a certified user")
 def getCertificate(email:str, db=Depends(crud.get_db)):
     user = crud.get_user(email, db)
+    if not user:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "User not found")
 
     # check if the user has a certificate 
     hasCertificate: bool = crud.certify(user)
